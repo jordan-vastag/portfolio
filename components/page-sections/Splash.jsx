4814d705476f3034648881a20637e358ef186b12
jordan-vastag <jvastag44@gmail.com>
2022-08-14 13:56:57 -0500

    added splash.css

--- components/page-sections/Splash.jsx
+++ components/page-sections/Splash.jsx
@@ -1,7 +1,15 @@
+import './Splash.module.css'
+
 const Splash = () => {
   return (
-    <div className="flex-column basis-full">
-      <p className="text-7xl border-red-500">Hi, I'm Jordan Vastag</p>
+    <div className="splash-root">
+      <div className="left-col">
+        
+      </div>
+      <div className="right-col">
+        <p className="text-7xl border-red-500">Hi, I'm Jordan Vastag</p>
+
+      </div>
     </div>
   );
 };
