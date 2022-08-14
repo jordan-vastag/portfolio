4e45e4c1c3fc96c89da9cda297e56a9ad6ad96a2
Jordan Vastag <110429362+jordan-vastag@users.noreply.github.com>
2022-08-14 13:33:19 -0500

    Delete site directory

--- site/pages/index.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline" style={styles}>Welcome home.</h1>
-  );
-}
