aef420f5d37459f2ffe94804dc341ae0799879c9
Jordan Vastag <110429362+jordan-vastag@users.noreply.github.com>
2022-08-17 17:37:46 -0500

    Delete src directory

--- src/sections/js/About.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/about.module.scss';
-
-const About = () => {
-  return <div className={styles.root}></div>;
-};
-
-export default About;
--- src/sections/js/Projects.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/projects.module.scss';
-
-const Projects = () => {
-  return <div className={styles.root}></div>;
-};
-
-export default Projects;
